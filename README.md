586c23758c7a53f3eeb30331fdd4616750e7104f
igasm <iga.smarz@gmail.com>
2019-07-16 17:36:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-zobpx9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-zobpx9)
---
+++ src/app/shopping-list/ingredient-component/ingredient-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredient-component',
+  templateUrl: './ingredient-component.component.html',
+  styleUrls: ['./ingredient-component.component.css']
+})
+export class IngredientComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
