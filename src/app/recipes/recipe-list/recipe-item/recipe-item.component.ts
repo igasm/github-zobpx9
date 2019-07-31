7323339642000c63834ad26bd31207eec34b39fa
igasm <iga.smarz@gmail.com>
2019-07-31 11:22:10 +0200

    passing data with event and property binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../Recipe.model';
 
 @Component({
@@ -8,10 +8,15 @@ import { Recipe } from '../../Recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipeItem: Recipe;
+  @Output() recipeItemSelected = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick() {
+    this.recipeItemSelected.emit();
   }
 
 }
